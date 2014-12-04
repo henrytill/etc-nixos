389385ed655b1ab383dc72e2fe0df4e71f201690
Henry Till <henrytill@gmail.com>
2014-12-04 11:58:10 -0500

    common: override dunst expression's patch phase

--- common.nix
+++ common.nix
@@ -46,6 +46,14 @@
 
   nixpkgs.config = {
     allowUnfree = true;
+
+    packageOverrides = pkgs: with pkgs; rec {
+
+      dunst = stdenv.lib.overrideDerivation pkgs.dunst (oldAttrs: {
+        patchPhase = null;
+      });
+
+    };
   };
 
 }
