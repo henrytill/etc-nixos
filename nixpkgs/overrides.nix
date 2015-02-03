bdbaaaa2714a20514dde7ca568fd57ee9209da50
Henry Till <henrytill@gmail.com>
2015-02-02 22:05:29 -0500

    haskellEnv: add hlint

--- nixpkgs/overrides.nix
+++ nixpkgs/overrides.nix
@@ -3,6 +3,7 @@ super: let self = super.pkgs; in {
   haskellEnv = self.haskellngPackages.ghcWithPackages (p: with p; [
     cabal-install
     cabal2nix
+    hlint
     xmobar
     xmonad
     xmonad-contrib
