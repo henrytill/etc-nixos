60d7632e194d07f86f44f0b35375da03268e3d9d
Henry Till <henrytill@gmail.com>
2015-02-11 23:22:25 -0500

    common: switch to zsh

--- common.nix
+++ common.nix
@@ -5,21 +5,6 @@ with lib;
 {
   imports = import ./modules/module-list.nix;
 
-  environment.etc."bashrc.local".text = ''
-    if [ -z "$IN_NIX_SHELL" ]; then
-        update_title () {
-            printf "\e]0;$USER@$HOSTNAME: $BASH_COMMAND\a"
-        }
-
-        case "$TERM" in
-        xterm*|rxvt*)
-            printf "\e]0;$USER@$HOSTNAME:\a"
-            trap update_title DEBUG
-            ;;
-        esac
-    fi
-  '';
-
   environment.systemPackages = with pkgs; [
     aspell
     aspellDicts.en
@@ -58,32 +43,9 @@ with lib;
 
   nixpkgs.config = import ./nixpkgs/config.nix;
 
-  programs.bash = {
-    interactiveShellInit = ''
-      p() {
-          clear
-          echo 'Current Profile: ' && readlink "$HOME/.nix-profile"
-          echo && echo 'Installed:' && nix-env -q
-      }
-    '';
-
-    shellAliases = {
-      e = "$EDITOR";
-      l = "clear && pwd && ls -lh";
-      la = "clear && pwd && ls -lah";
-      lf = "ls -aF";
-      ll = "ls -la";
-      llt = "ls -lat";
-      ls = "ls --color=tty";
-      lt = "ls -lt";
-      restart = "systemctl restart";
-      start = "systemctl start";
-      status = "systemctl status";
-      stop = "systemctl stop";
-      u = "cd .. && l";
-      which = "type -P";
-    };
-  };
+  programs.zsh.enable = true;
 
   programs.emacs.enable = true;
+
+  users.defaultUserShell = "/run/current-system/sw/bin/zsh";
 }
