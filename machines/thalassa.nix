2e814ba18b96383c4f1b1c37b68e5318e2dfc2d8
Henry Till <henrytill@gmail.com>
2015-11-17 21:47:16 -0500

    machines/thalassa: use pulseaudio

--- machines/thalassa.nix
+++ machines/thalassa.nix
@@ -9,10 +9,19 @@
   boot.loader.gummiboot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
+
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
+  };
+
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
   };
 
   ht.conky.enable = true;
