6368e6f1e176967baee348d2d21dd9137442071e
Henry Till <henrytill@gmail.com>
2015-11-23 14:56:46 -0500

    machines/thalassa: add zfs config

--- machines/thalassa.nix
+++ machines/thalassa.nix
@@ -6,12 +6,23 @@
       ../desktop.nix
     ];
 
+  boot.initrd.availableKernelModules = [ "uas" ];
   boot.loader.gummiboot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs = {
+    forceImportAll = false;
+    forceImportRoot = false;
+  };
 
   environment.systemPackages = with pkgs; [
     pavucontrol
   ];
+
+  fileSystems."/mnt/vms" = {
+    device = "ext/vms";
+    fsType = "zfs";
+  };
 
   i18n = {
     consoleFont = "Lat2-Terminus16";
@@ -27,6 +38,7 @@
   ht.conky.enable = true;
 
   networking.hostName = "thalassa";
+  networking.hostId = "91855dd5";
 
   services.xserver.xkbOptions = "ctrl:nocaps";
 
