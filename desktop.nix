42283a864e170a34cd7b5d6123e8b785758b2b2a
Henry Till <henrytill@gmail.com>
2015-11-28 01:29:49 -0500

    desktop: clean up fonts.fontconfig

--- desktop.nix
+++ desktop.nix
@@ -24,15 +24,17 @@ in {
   fonts = {
     enableFontDir = true;
     enableGhostscriptFonts = true;
-    fontconfig.antialias = true;
-    fontconfig.enable = true;
-    fontconfig.hinting.autohint = false;
-    fontconfig.hinting.enable = true;
-    fontconfig.hinting.style = "slight";
-    fontconfig.includeUserConf = false;
-    fontconfig.subpixel.lcdfilter = "default";
-    fontconfig.subpixel.rgba = "rgb";
-    fontconfig.ultimate.enable = true;
+    fontconfig = {
+      antialias = true;
+      enable = true;
+      hinting.autohint = false;
+      hinting.enable = true;
+      hinting.style = "slight";
+      includeUserConf = false;
+      subpixel.lcdfilter = "default";
+      subpixel.rgba = "rgb";
+      ultimate.enable = true;
+    };
     fonts = with pkgs; [
       mplus-outline-fonts
     ];
