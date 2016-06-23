912a7a777751eb0bffeaaf4a819f8e5932b30e30
Henry Till <henrytill@gmail.com>
2016-06-23 15:32:17 -0400

    development: update

--- development.nix
+++ development.nix
@@ -1,24 +1,13 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 {
   environment.systemPackages = with pkgs; [
-    activator
-    boot
+    coq
     haskellPackages.cabal-install
     haskellPackages.cabal2nix
     haskellPackages.ghc
     # haskellPackages.hasktags
-    jdk
     jshon
-    leiningen
-    maven
-    nodejs
     # pandoc
-    phantomjs2
-    sbt
   ];
-
-  environment.variables.JAVA_HOME = "${pkgs.jdk}/lib/openjdk";
 }
